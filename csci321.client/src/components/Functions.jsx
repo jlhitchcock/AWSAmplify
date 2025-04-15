6e171ac62058625f0a561ba36e22d58addb96b52
jlhitchcock <113740147+jlhitchcock@users.noreply.github.com>
2025-04-15 21:20:58 +1000

    1

--- csci321.client/src/components/Functions.jsx
+++
@@ -1,9 +0,0 @@
-﻿
-export const generateObjectId = () =>  {
-    const timestamp = (Math.floor(new Date().getTime() / 1000)).toString(16);
-    const randomHex = 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, function() {
-        return (Math.floor(Math.random() * 16)).toString(16);
-    });
-
-    return timestamp + randomHex;
-}
