58ca40615ecf65518b41fc368254a540d3b8b2cf
jlhitchcock <113740147+jlhitchcock@users.noreply.github.com>
2025-04-15 21:35:32 +1000

    fix typo

--- src/components/InterestedPage.jsx
+++ src/components/InterestedPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import "./interestedPage.css";
+import "./InterestedPage.css";
 
 
 const InterestedPage = ({ interests, onInterestsChange }) => {
